9e1e0af447929827dba01ac559f70b2763b45687
Ömer Faruk Genç <omergnc06@gmail.com>
2021-06-25 16:04:15 +0300

    animasyon eklendi.

---
+++ lib/animasyon.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+class FadeAnimation extends StatelessWidget {
+  final double delay;
+  final Widget child;
+
+  FadeAnimation(this.delay, this.child);
+
+  @override
+  Widget build(BuildContext context) {
+    final tween = MultiTrackTween([
+      Track("opacity").add(Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0)),
+      Track("translateY").add(
+          Duration(milliseconds: 500), Tween(begin: -30.0, end: 0.0),
+          curve: Curves.easeOut)
+    ]);
+
+    return ControlledAnimation(
+      delay: Duration(milliseconds: (500 * delay).round()),
+      duration: tween.duration,
+      tween: tween,
+      child: child,
+      builderWithChild: (context, child, animation) => Opacity(
+        opacity: animation["opacity"],
+        child: Transform.translate(
+            offset: Offset(0, animation["translateY"]),
+            child: child
+        ),
+      ),
+    );
+  }
+  Widget makeInput({label, obscureText = false}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(label, style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+            color: Colors.black87
+        ),),
+        SizedBox(height: 5,),
+        TextField(
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey[400])
+            ),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey[400])
+            ),
+          ),
+        ),
+        SizedBox(height: 30,),
+      ],
+    );
+  }
+}
--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vize/animasyon.dart';
 import '../servisler/girisservis.dart';
 class LoginSayfasi extends StatefulWidget {
   @override
@@ -24,104 +25,106 @@ class _LoginSayfasiState extends State<LoginSayfasi> {
             ),
             SingleChildScrollView(
               child: new Center(
-                child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Padding(padding: EdgeInsets.all(35)),
+                child: FadeAnimation(2,
+                   Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    Padding(padding: EdgeInsets.all(35)),
 
-                  Text(
-                    "Giriş Yap",
-                    style: TextStyle(
-                        fontSize: 50,
-                        color: Color.fromARGB(255, 10, 31, 67),
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Padding(padding: EdgeInsets.all(135)),
-                  TextField(
-                    controller: mail,
-                    decoration: InputDecoration(
-                      hintText: 'Mail Adresinizi Giriniz',
-                      prefixIcon: Icon(Icons.email),
-                      hintStyle: TextStyle(color: Colors.blue),
-                      filled: true,
-                      fillColor: Colors.white,
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                        borderSide: BorderSide(color: Colors.green, width: 2),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(color: Colors.green, width: 2),
-                      ),
-                    ),
-                  ),
-
-                  Padding(padding: EdgeInsets.all(3)),
-                  TextField(
-                    controller: sifre,
-                    autocorrect: true,
-                    decoration: InputDecoration(
-                      hintText: 'Şifrenizi Giriniz',
-                      prefixIcon: Icon(Icons.vpn_key),
-                      hintStyle: TextStyle(color: Colors.blue),
-                      filled: true,
-                      fillColor: Colors.white,
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                        borderSide: BorderSide(color: Colors.blue, width: 2),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(color: Colors.green, width: 2),
-                      ),
-                    ),),
-
-                  Padding(padding: EdgeInsets.all(10)),
-                  FlatButton.icon(
-                    icon: Icon(Icons.person,color: Colors.white,),
-                    height: 58,
-                    minWidth: 340,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(12)),
-                    label: Text(
+                    Text(
                       "Giriş Yap",
                       style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-
+                          fontSize: 50,
+                          color: Color.fromARGB(255, 10, 31, 67),
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Padding(padding: EdgeInsets.all(135)),
+                    TextField(
+                      controller: mail,
+                      decoration: InputDecoration(
+                        hintText: 'Mail Adresinizi Giriniz',
+                        prefixIcon: Icon(Icons.email),
+                        hintStyle: TextStyle(color: Colors.blue),
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                          borderSide: BorderSide(color: Colors.green, width: 2),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(color: Colors.green, width: 2),
+                        ),
                       ),
                     ),
-                    color: Color.fromARGB(255, 14, 46, 99),
-                    onPressed: () {
-                      _giris.signIn(mail.text, sifre.text).then((value) {Navigator.pushNamed(context, "/vize");});
-                      //Navigator.pushNamed(context, "/settings");
-                    },
-                  ),
-                  Padding(padding: EdgeInsets.all(5)),
-                  FlatButton.icon(
-                    icon: Icon(Icons.person,color: Colors.white,),
-                    height: 58,
-                    minWidth: 340,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(12)),
-                    label: Text(
-                      "Şifremi Unuttum",
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
 
+                    Padding(padding: EdgeInsets.all(3)),
+                    TextField(
+                      controller: sifre,
+                      autocorrect: true,
+                      decoration: InputDecoration(
+                        hintText: 'Şifrenizi Giriniz',
+                        prefixIcon: Icon(Icons.vpn_key),
+                        hintStyle: TextStyle(color: Colors.blue),
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                          borderSide: BorderSide(color: Colors.blue, width: 2),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(color: Colors.green, width: 2),
+                        ),
+                      ),),
+
+                    Padding(padding: EdgeInsets.all(10)),
+                    FlatButton.icon(
+                      icon: Icon(Icons.person,color: Colors.white,),
+                      height: 58,
+                      minWidth: 340,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(12)),
+                      label: Text(
+                        "Giriş Yap",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+
+                        ),
                       ),
+                      color: Color.fromARGB(255, 14, 46, 99),
+                      onPressed: () {
+                        _giris.signIn(mail.text, sifre.text).then((value) {Navigator.pushNamed(context, "/vize");});
+                        //Navigator.pushNamed(context, "/settings");
+                      },
                     ),
-                    color: Color.fromARGB(255, 14, 46, 99),
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/sifir");
-                      //Navigator.pushNamed(context, "/settings");
-                    },
-                  ),
-                  Padding(padding: EdgeInsets.all(5)),
+                    Padding(padding: EdgeInsets.all(5)),
+                    FlatButton.icon(
+                      icon: Icon(Icons.person,color: Colors.white,),
+                      height: 58,
+                      minWidth: 340,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(12)),
+                      label: Text(
+                        "Şifremi Unuttum",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+
+                        ),
+                      ),
+                      color: Color.fromARGB(255, 14, 46, 99),
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/sifir");
+                        //Navigator.pushNamed(context, "/settings");
+                      },
+                    ),
+                    Padding(padding: EdgeInsets.all(5)),
 
 
-                ]),
+                  ]),
+                ),
               ),
             )
           ],
